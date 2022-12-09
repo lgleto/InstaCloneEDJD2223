a7344a0bae19d73e6cb8f6fae741c59314a24bbe
lourencogomes <leto.lg@gmail.com>
2022-12-09 17:06:27 +0000

    first commit

---
+++ app/src/main/java/ipca/example/instaclone/Photo.kt
@@ -0,0 +1,36 @@
+package ipca.example.instaclone
+
+import com.google.firebase.firestore.DocumentSnapshot
+import java.util.*
+
+class Photo {
+
+    var description : String? = null
+    var photo : String? = null
+
+    constructor(description: String?, photo: String?) {
+        this.description = description
+        this.photo = photo
+    }
+
+
+    fun toHashMap () : HashMap<String,Any?> {
+       return hashMapOf(
+           "description" to description,
+           "photo" to  photo
+       )
+    }
+
+    companion object {
+        fun fromDoc (doc:DocumentSnapshot) :Photo {
+            return Photo (
+                doc.getString("description"),
+                doc.getString("photo")
+            )
+        }
+    }
+
+
+
+
+}
